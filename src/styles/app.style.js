d1b7a99d00518f7068b608ae5a3856ab56a8d243
smokerigni <smokerigni@gmail.com>
2018-04-14 11:10:14 +0200

    added normalize style

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -9,14 +9,7 @@ s.root = {
   letterSpacing: '0.025em',
   padding: '3vh 0 12vh 0',
   width: '500px',
-  // use responsive max-width to simulate padding/margin to allow
-  // space for vertical scroll bar without creating horizontal scroll bar
-  // (if there is padding, the window will scroll horizontally to show the padding)
   maxWidth: 'calc(100vw - 40px)',
-
-  // center based on vw to prevent content jump when vertical scroll bar show/hide
-  // note: vw/vh include the width of scroll bars. Note that centering using margin auto
-  // or % (which doesn't include scroll bars, so changes when scroll bars shown) causes a page jump
   position: 'relative',
   left: '50vw',
   WebkitTransform: 'translate(-50%, 0)',
@@ -24,7 +17,6 @@ s.root = {
   msTransform: 'translate(-50%, 0)',
   OTransform: 'translate(-50%, 0)',
   transform: 'translate(-50%, 0)',
-
   WebkitTextSizeAdjust: 'none',
   MozTextSizeAdjust: 'none',
   msTextSizeAdjust: 'none',
